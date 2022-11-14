74c1a703c74d042428fb94d3615d741c10c00c28
Nma Shawkat <nmashawket@gmail.com>
2022-11-14 12:31:26 +0300

    donee

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+//   let form = document.querySelector('form')
+//   form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     console.log(e.target.description.value)
+//     // buildTodoList(e.target.description.value)
+//     // form.reset()
+
+//   }) 
+  
+// });
+
+// function buildTodoList (todo) {
+//   let p = document.createElement('p')
+//   let btn = document.createElement('btn')
+//   btn.addEventListener('click', handleDelete)
+//   btn.textContent= 'x'
+//   p.textContent=`${todo} `
+//   p.appendChild(btn)
+//   console.log(p)
+//   document.querySelector('#todo_container').appendChild(p)
+// }
+// function handleDelete(e){
+//   e.target.parentNode.remove()
+// }
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    console.log(e.target)
+  }) 
+  
 });
